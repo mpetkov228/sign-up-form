13a543763413a9538a280cfd47a95f36194ee5bc
mpetkov228 <mpetkov228@gmail.com>
2023-08-02 16:43:55 +0300

    Add password checker function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello');
+function checkPasswords(form) {
+    let pass = form.pass.value;
+    let repass = form.repass.value;
+    
+    if (pass == '') {
+        alert('Please enter password!');
+        return false;
+    } else if (repass == '') {
+        alert('Please enter confirm password!');
+        return false;
+    } else if (pass != repass) {
+        alert('Passwords do not match!');
+        return false;
+    } else {
+        alert('Success!');
+        return true;
+    }
+}
